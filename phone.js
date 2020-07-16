2a938d5cbfdf5d5a3542e4da49f0840343e7514e
Vincent Bushong <vinbush@gmail.com>
2020-07-16 07:53:14 -0500

    Assignment 2

--- phone.js
+++ phone.js
@@ -9,4 +9,36 @@ $(".tab-switch-btn").click(function() {
     var selector = $(this).data("target")
     $(".tab").hide();
     $(selector).show();
-})
+});
+
+$(".dialer-btn.number-btn").click(function() {
+    var oldNum = $("#dialer-number").val();
+    var newNum = $(this).val();
+    $("#dialer-number").val(oldNum + newNum);
+});
+
+$("#clear-btn").click(function() {
+    $("#dialer-number").val("");
+});
+
+// yeah global variables because handling state in javascript sucks
+var downX = 0;
+var downY = 0;
+
+$("#gesture-area").mousedown(function(event) {
+    $("#gesture-output").val("mouse down");
+    downX = event.pageX;
+    downY = event.pageY;
+});
+
+$("#gesture-area").mouseup(function(event) {
+    $("#gesture-output").val("mouse up");
+    upX = event.pageX;
+    upY = event.pageY;
+
+    if (upX < downX) {
+        $("#gesture-output").val("swipe left")
+    } else if (upX > downX) {
+        $("#gesture-output").val("swipe right")
+    }
+});
